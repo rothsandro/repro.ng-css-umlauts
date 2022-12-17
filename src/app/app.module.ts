ffb372acaa383007ed41a2efb7f81f714d52c7b1
Sandro Roth <sandroroth@outlook.com>
2022-12-17 13:49:03 +0100

    Add example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShadowDomComponent } from './shadow-dom/shadow-dom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShadowDomComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shadow-dom/shadow-dom.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-shadow-dom',
+  templateUrl: './shadow-dom.component.html',
+  styleUrls: ['./shadow-dom.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom,
+})
+export class ShadowDomComponent {}
